327dbcf77dc13a160b3305e2b72f5d554bf3dd97
Alessio Del Frate <65942952+Alessio-7@users.noreply.github.com>
2021-08-13 16:03:34 +0200

    Delete src directory

--- src/utility/WrongValueException.java
+++
@@ -1,9 +0,0 @@
-package utility;
-
-public class WrongValueException extends Exception {
-	private static final long serialVersionUID = -9048508152870964929L;
-
-	public WrongValueException( String errorMessage ) {
-		super( errorMessage );
-	}
-}
